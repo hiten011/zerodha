1e40c968c8370b008aa3e81281341194c1fcfa06
hiten <hitengupta12345@gmail.com>
2025-07-23 00:17:08 +0930

    created Education.jsx

--- frontend/src/landing_page/home/Education.jsx
+++ frontend/src/landing_page/home/Education.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React from "react";
+import Card from "../Card";
 
-function Education(){
-    return (
-        <h1>Education</h1>
-    );
+function Education() {
+	return (
+		<Card
+			left={
+				<div className="d-flex justify-content-center align-items-center">
+					<img src="media/images/education.svg" style={{ width: "80%" }} />
+				</div>
+			}
+
+			right={
+				<>
+					<h1 className="mb-3 fs-2">Free and open market education</h1>
+					<p>
+						Varsity, the largest online stock market education book in the world
+						covering everything from the basics to advanced trading.
+					</p>
+					<a href="" style={{ textDecoration: "none" }}>
+						Versity <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+					</a>
+					<p className="mt-5">
+						TradingQ&A, the most active trading and investment community in
+						India for all your market related queries.
+					</p>
+					<a href="" style={{ textDecoration: "none" }}>
+						TradingQ&A <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+					</a>
+				</>
+			}
+		/>
+	);
 }
 
 export default Education;
